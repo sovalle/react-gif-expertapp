9ef78b373f6e02d325677d2deba931f76a1c8ed5
Sebastian Ovalle <sovalle@MacBook-Pro-de-Sebastian.local>
2020-10-26 00:03:37 -0300

    Add docs

---
+++ docs/precache-manifest.ad9cf98738c3da300a7b2a17f9c79e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0d21ed802fa477fe816aef46735d0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8835339a593687d44946",
+    "url": "/static/css/main.2a65f073.chunk.css"
+  },
+  {
+    "revision": "78241dda10065ec56559",
+    "url": "/static/js/2.94b5915f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.94b5915f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8835339a593687d44946",
+    "url": "/static/js/main.3c8f731a.chunk.js"
+  },
+  {
+    "revision": "19fbb0bacf64dc9be424",
+    "url": "/static/js/runtime-main.bf9ebc5d.js"
+  }
+]);
